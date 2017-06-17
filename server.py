b0695525f3b8e43db1aa4844fdf89b2d1ea5c8b0
Nik Vangerow <n@notro.uk>
2017-06-17 20:27:08 +0100

    Now it talks back to you

--- server.py
+++ server.py
@@ -1,15 +1,35 @@
 from flask import Flask
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-@ask.intent('HelloIntent')
-def hello(firstname):
-    speech_text = "Hello %s" % firstname
-    return statement(speech_text).simple_card('Hello', speech_text)
+please_text = "<speak>please <emphasis level=\"reduced\">please</emphasis> <emphasis level=\"moderate\">please</emphasis> <emphasis level=\"strong\">please</emphasis></speak>"
+
+@ask.launch
+def launch():
+    return question("Say hello").reprompt(please_text)
+
+@ask.intent('Hello')
+def hello(name):
+    speech_text = "Nice to meet you, {}".format(name)
+    return statement(speech_text).simple_card('Hi', speech_text)
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    return question(please_text)
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    return statement("Cancelled")
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    return statement("Stopped")
+
+@ask.session_ended
+def session_ended():
+    return statement("Bye bye lol")
 
 if __name__ == '__main__':
-    context = ('cert/server.crt', 'cert/server.key')
-    app.run(host='0.0.0.0', port=443, ssl_context=context, threaded=True,
-            debug=True)
+    app.run(host='0.0.0.0', port=5000)
